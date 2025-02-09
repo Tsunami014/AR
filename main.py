77a2170b7db734ad40386fb7922c1cc7df856f10
Tsunami014 (Max) <96847801+Tsunami014@users.noreply.github.com>
2025-02-10 10:21:17 +1100

    Made the 3d environment moveinable

--- main.py
+++ main.py
@@ -46,7 +46,9 @@ def main():
 
     gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
 
-    glTranslatef(0.0,0.0, -5)
+    glTranslatef(0,0, -10)
+
+    glRotatef(25, 2, 1, 0)
 
     while True:
         for event in pygame.event.get():
@@ -54,7 +56,25 @@ def main():
                 pygame.quit()
                 quit()
 
-        glRotatef(1, 3, 1, 1)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                    glTranslatef(0.5,0,0)
+                if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                    glTranslatef(-0.5,0,0)
+
+                if event.key == pygame.K_UP or event.key == pygame.K_w:
+                    glTranslatef(0,-1,0)
+                if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                    glTranslatef(0,1,0)
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 4:
+                    glTranslatef(0,0,1.0)
+
+                if event.button == 5:
+                    glTranslatef(0,0,-1.0)
+
+        #glRotatef(1, 3, 1, 1)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         Cube()
         pygame.display.flip()
